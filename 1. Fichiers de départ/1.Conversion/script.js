a180a45c7a0ccbceb02567c34c7f58cd20d556fe
cedricVrvrd <cedric.immo@live.fr>
2023-01-29 16:26:23 +0100

    40 exercises algo JS

---
+++ 1. Fichiers de départ/1.Conversion/script.js
@@ -0,0 +1,25 @@
+function conversion(deg) {
+
+}
+
+
+/* ÉNONCÉ 📚 */
+
+
+/* 
+Créez un algorithme qui sert à convertir des degrés Celsius en degrés Fahrenheit.
+Le résultat doit être de type nombre, sans chiffre après la virgule.
+Si on donne un autre type qu'un nombre en argument, vous devez retourner "Données en entrée non-correctes."
+
+Formule pour convertir les degrés Celsius en degrés Fahrenheit : (degCel * (9/5) + 32);
+*/
+
+
+/* Tests à passer 🧪 */
+
+console.log(conversion(-37));       // -34
+console.log(conversion(14));        // 57
+console.log(conversion(0));         // 32
+console.log(conversion("abc"));     // Données en entrée non-correctes.
+
+
